258d14f917dfd59022d73a02c349dc9779e9f661
Sebastian <sebastian.franz@tum.de>
2024-11-07 15:31:48 +0100

    Adding FLIP Dataset splits as dictionary

---
+++ autoeval/utilities/FLIP.py
@@ -0,0 +1,50 @@
+FLIP_DATASETS = {
+    "aav": [
+        "des_mut",
+        "mut_des",
+        "one_vs_many",
+        "two_vs_many",
+        "seven_vs_many",
+        "low_vs_high",
+        "sampled"
+    ],
+    "meltome": [
+        "mixed_split",
+        "human",
+        "human_cell"
+    ],
+    "gb1": [
+        "one_vs_rest",
+        "two_vs_rest",
+        "three_vs_rest",
+        "low_vs_high",
+        "sampled"
+    ],
+    "scl": [
+        "mixed_soft",
+        "mixed_hard",
+        "human_soft",
+        "human_hard",
+        "balanced",
+        "mixed_vs_human_2"
+    ],
+    "bind": [
+        "one_vs_many",
+        "two_vs_many",
+        "from_publication",
+        "one_vs_sm",
+        "one_vs_mn",
+        "one_vs_sn"
+    ],
+    "sav": [
+        "mixed",
+        "human",
+        "only_savs"
+    ],
+    "secondary_structure": [
+        ""
+    ],
+    "conservation": [
+        ""
+    ]
+}
